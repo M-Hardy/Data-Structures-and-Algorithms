6e7ff05bbe2efcbc2cdaebf8d2155f09abc9aa1e
Michael <mbhardy001@gmail.com>
2024-02-08 11:52:31 -0500

    Removed toLowerCase() call on entire string and applied to individual char comparisons.

--- neetcode_solutions/two_pointers/validPalindrome.js
+++ neetcode_solutions/two_pointers/validPalindrome.js
@@ -2,27 +2,26 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
 
-    s = s.toLowerCase();
-    let start = 0;
-    let end = s.length - 1;
 
-    while (start < end) {
-        while (!s[start].match(/^[0-9a-z]$/)) {
-            ++start;
-            if (start >= end) return true;
+const isPalindrome = function(s) {
+
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+        while (l < r && !s[l].toLowerCase().match(/^[0-9a-z]$/)) {
+            ++l;
         }
 
-        while (!s[end].match(/^[0-9a-z]$/)) {
-            --end;
-            if (end <= start) return true;
+        while (r > l && !s[r].toLowerCase().match(/^[0-9a-z]$/)) {
+            --r;
         }
 
-        if (s[start] !== s[end]) return false;
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
 
-        ++start;
-        --end;
+        ++l;
+        --r;
     }
     return true;
 };
