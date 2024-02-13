726e4ae2bd97ac9c8457ed638c46c77ef55fbd0c
Michael <mbhardy001@gmail.com>
2024-02-13 13:05:26 -0500

    Spacing edits.

--- neetcode_solutions/arrays_and_hashing/product_of_array_except_self.py/product_of_array_except_self.py
+++ neetcode_solutions/arrays_and_hashing/product_of_array_except_self.py/product_of_array_except_self.py
@@ -9,10 +9,9 @@ class Solution(object):
         for i in range(1, len(res)):
             res[i] *= res[i-1] * nums[i-1]
 
-        # back_accumulator = [1] * len(nums)
         back_accumulator = 1
         for i  in range(len(nums)-2,-1,-1):
             back_accumulator *= nums[i+1]
-            res[i] *= back_accumulator[i]
+            res[i] *= back_accumulator
 
         return res
