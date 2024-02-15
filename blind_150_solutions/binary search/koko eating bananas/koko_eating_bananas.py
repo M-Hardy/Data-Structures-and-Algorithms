8cce46085bd9a7bc4528d2702321c6579ef583eb
Michael <mbhardy001@gmail.com>
2024-02-15 15:04:11 -0500

    Add initial koko eating bananas solution.

---
+++ blind_150_solutions/binary search/koko eating bananas/koko_eating_bananas.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        
+        l = 1
+        r = max(piles)
+        min_k = r
+
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
+
+            for pile in piles:
+                hours_for_pile = (pile // k) + (0 if pile % k == 0 else 1)
+                hours += hours_for_pile
+                if hours > h:
+                    l = k + 1
+                    break
+
+            if hours <= h:
+                r = k -1
+                min_k = min(min_k, k)
+
+        return min_k
+    
+test = Solution()
+print(test.minEatingSpeed([3,6,7,11], 8))
